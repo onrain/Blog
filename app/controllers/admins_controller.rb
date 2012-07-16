4c794eab9154c65b28dfd2f98061a9f595ea6f81
Onrain <prostirchas@gmail.com>
2012-07-16 16:53:30 +0300

    Restfull api

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,90 +1,59 @@
 class AdminsController < ApplicationController
   before_filter :authorize
   
+  respond_to :html, :json, :xml
+  
   def index
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @admins }
-    end
+  
   end
   
   def show
     @admin = Admin.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @admin }
-    end
+    respond_with @admin
   end
   
   def comments
     @comment = Comment.paginate page: params[:page], order: 'data_p desc', per_page: 10
+    respond_with @comment
   end
   
   def published
     @admins_p = Admin.where(:published => 1).paginate page: params[:page], order: 'published desc', per_page: 10
+    respond_with @admins_p
   end
 
   
   def npublished
     @admins_no_p = Admin.where(:published => 0).paginate page: params[:page], order: 'published desc', per_page: 10
+    respond_with @admins_no_p
   end
 
   def new
     @admin = Admin.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @admin }
-    end
   end
 
-  # GET /admins/1/edit
   def edit
     @admin = Admin.find(params[:id])
   end
 
-  # POST /admins
-  # POST /admins.json
   def create
     @admin = Admin.new(params[:admin])
-    
-    expire_page :action => :index, :action=> :show, :controller=>:comments
-    
-    respond_to do |format|
-      if @admin.save
-        format.html { redirect_to @admin, notice: 'Admin was successfully created.' }
-        format.json { render json: @admin, status: :created, location: @admin }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @admin.errors, status: :unprocessable_entity }
-      end
-    end
+    @admin.save
+    respond_with @admin
   end
 
 
   def update
     @admin = Admin.find(params[:id])
-
-    respond_to do |format|
-      if @admin.update_attributes(params[:admin])
-        format.html { redirect_to @admin, notice: 'Admin was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @admin.errors, status: :unprocessable_entity }
-      end
-    end
+    @admin.update_attributes(params[:admin])
+    respond_with @admin
   end
 
 
   def destroy
     @admin = Admin.find(params[:id])
     @admin.destroy
-
-    respond_to do |format|
-      format.html { redirect_to request.env['HTTP_REFERER'], notice: "Deleted success." }
-      format.json { head :no_content }
-    end
+    respond_with @admin
   end
   
 protected
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,36 +2,29 @@ class CommentsController < ApplicationController
   
   caches_page :index, :show, :gzip => :best_speed
   
+  respond_to :html, :json, :xml
+  
   def index    
     @articles = Admin.where(:published => 1).paginate page: params[:page], order: 'data_create desc', per_page: 10
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
+    respond_with @articles
   end
 
 
   def show
-   
     @articles = Admin.find(params[:id])
     @comments_q = Comment.find_all_by_post_id(params[:id], :order => "data_p desc").paginate page: params[:page], order: 'data_p desc', per_page: 5
     @comment = Comment.new
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
+    respond_with @articles
   end
 
 
   def create
     @comment = Comment.new(params[:comment])
     Time.zone = "Kyiv"
-    @comment.data_p = Time.zone.now
-    respond_to do |format|
+    @comment.data_p = Time.zone.now 
+    respond_with(@comment) do |format|
       if @comment.save
-        format.html { redirect_to request.env['HTTP_REFERER'], notice: 'Comment was successfully created.' }
+        format.html { redirect_to request.env['HTTP_REFERER'], alert: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html {redirect_to request.env['HTTP_REFERER'], notice: "Please leave your comment." }
@@ -44,10 +37,6 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to admins_comments_url }
-      format.json { head :no_content }
-    end
+    respond_with @comment
   end
 end
